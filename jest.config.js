b88a3ba272d5bb716280a5318e5f1822728a23d6
Jake Perez <501068@containerstore.com>
2020-01-30 20:36:39 -0600

    Set up initial repo

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
+  collectCoverageFrom: ['**/*.ts', '!**/__data__/**'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
